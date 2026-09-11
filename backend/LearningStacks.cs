eca2c08adf1b7fcda594b0de5fd68a2037e4ac3c
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-09-11 10:46:31 +0200

    Remove unused csv, and setup a proper example using the dataset we're using

--- backend/LearningStacks.cs
+++ backend/LearningStacks.cs
@@ -6,13 +6,13 @@ public static class LearningStacks
 {
     public static double ExampleStack()
     {
-        string dataPath = "housing.csv";
+        string dataPath = "screen_time_mental_health.csv";
 
         //Step 1. Create an ML Context
         var ctx = new MLContext();
 
         //Step 2. Read in the input data from a text file for model training
-        IDataView trainingData = ctx.Data.LoadFromTextFile<ModelInput>(dataPath, hasHeader: true, separatorChar: ',');
+        IDataView trainingData = ctx.Data.LoadFromTextFile<SleepInput>(dataPath, hasHeader: true, separatorChar: ',');
 
         // Step 3. Split the data into training and test sets
         var split = ctx.Data.TrainTestSplit(trainingData,testFraction: 0.2);
@@ -22,10 +22,10 @@ public static class LearningStacks
         // Step 4. Build the data processing and training pipeline
         var pipeline = ctx.Transforms
             .Concatenate("Features",
-                nameof(ModelInput.population),
-                nameof(ModelInput.housing_median_age))
+                nameof(SleepInput.sleep_quality_index),
+                nameof(SleepInput.avg_sleep_hours))
             .Append(ctx.Regression.Trainers.Sdca(
-                labelColumnName: nameof(ModelInput.median_house_value)));
+                labelColumnName: nameof(SleepInput.bdi_total)));
 
         // Step 5. Train the model
         ITransformer trainedModel = pipeline.Fit(trainData);
@@ -34,7 +34,7 @@ public static class LearningStacks
         IDataView predictions = trainedModel.Transform(testData);
 
         // Step 7. Evaluate the model
-        var metrics = ctx.Regression.Evaluate(predictions,labelColumnName: nameof(ModelInput.median_house_value));
+        var metrics = ctx.Regression.Evaluate(predictions,labelColumnName: nameof(SleepInput.bdi_total));
 
         return metrics.RSquared;
     }
