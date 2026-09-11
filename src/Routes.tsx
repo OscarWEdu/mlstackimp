60aaeef1a1b674270f85bbce5708c89c8ad8b3fb
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-09-11 12:34:14 +0200

    Add a separate homepage for navigation

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,10 @@
 import type { RouteObject } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
+import Stat1Page from "./pages/Stat1.tsx";
 
 const routes: RouteObject[] = [
-	{ path: "/", element: <HomePage /> }
+	{ path: "/", element: <HomePage /> },
+	{ path: "/stat1", element: <Stat1Page /> }
 ];
 
 export default routes;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,33 +1,13 @@
-import { useEffect, useState } from "react";
-import "./../style.css";
-
-async function getBackendResponse(): Promise<string> {
-	const response = await fetch("/api");
-	
-	if (!response.ok) {
-		throw new Error(`Backend returned ${response.status}`);
-	}
-	
-	return await response.text();
-}
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
-	const [message, setMessage] = useState("Connecting...");
-	
-	useEffect(() => {
-		getBackendResponse()
-		.then((data) => {
-			setMessage(`Backend response: ${data}`);
-		})
-		.catch((error) => {
-			setMessage(`Backend error: ${error}`);
-			console.error(error);
-		});
-	}, []);
-	
+	const navigate = useNavigate();
+
 	return (
-		<section id="center">
-		<p id="backend-response">{message}</p>
-		</section>
+		<div>
+			<button onClick={() => navigate("/stat1")}>
+				Analysis 1
+			</button>
+		</div>
 	);
 }
