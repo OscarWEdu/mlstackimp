af00f3023c6f49965fe9e3f64153e35219d7f129
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-09-17 14:24:30 +0200

    More explicit testing

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,7 +8,7 @@ app.MapGet("/api", () => "Test Call");
 app.MapPost("/api/sleeppredict", (SleepRequest request) =>
 {
     //Trains associated model if a dump of it doesn't already exist
-    if (!File.Exists(LearningStacks.SleepTrainerModelPath)) { LearningStacks.SleepTrainer(); }
+    if (!File.Exists(LearningStacks.SleepTrainerModelPath) || !File.Exists(LearningStacks.SleepTrainerMaleModelPath) || !File.Exists(LearningStacks.SleepTrainerFemaleModelPath)) { LearningStacks.SleepTrainer(); }
 
     var bdi = Predictors.SleepBDIPrediction(
         request.SleepQualityIndex,
