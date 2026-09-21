e4f2fd4ec17a6f0ee11e8bd26371ad50f449fe7b
Jacob <jacoblasse4@gmail.com>
2026-09-21 11:17:55 +0200

    Link header logo to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import "./style.css";
 import logo from "./assets/MLSTACK.png";
 
@@ -8,7 +8,9 @@ export default function App() {
 	return (
 		<div className="flex min-h-screen flex-col overflow-x-hidden">
 			<header className="flex items-center justify-between px-6  border-b-2">
-				<img src={logo} alt="MlStack Logo" className="h-20 w-auto" />
+				<Link to="/" aria-label="Till startsidan" className="rounded transition-opacity hover:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--accent)]">
+					<img src={logo} alt="MlStack Logo" className="h-20 w-auto" />
+				</Link>
 			</header>
 		
 			<main className="container mx-auto flex flex-1 flex-col px-4">
